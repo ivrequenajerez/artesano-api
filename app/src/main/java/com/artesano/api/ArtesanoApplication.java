06f20f512b0cee7a0efe3ce65c87eefc9b2b5cfe
ianvalentino <ianrequenajerez@gmail.com>
2026-06-05 16:27:22 +0200

    Primer commit

---
+++ app/src/main/java/com/artesano/api/ArtesanoApplication.java
@@ -0,0 +1,12 @@
+package com.artesano.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtesanoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtesanoApplication.class, args);
+    }
+}
